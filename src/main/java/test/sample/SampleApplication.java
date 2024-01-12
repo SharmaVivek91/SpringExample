aaf7dc8e2581e4536e28c5f5b9172a1eaf7f40e5
viveksharma <52680905+SharmaVivek91@users.noreply.github.com>
2024-01-11 19:59:25 -0600

    Day1
    Creating initial web app with test cases

--- src/main/java/test/sample/SampleApplication.java
+++ src/main/java/test/sample/SampleApplication.java
@@ -3,10 +3,12 @@ package test.sample;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+//this annotation is comprised of @SpringBootConfiguration, @EnableAutoConfiguration and @ComponentScan
+//this is bootstrap class
 @SpringBootApplication
 public class SampleApplication {
-
 	public static void main(String[] args) {
+		//creates application context
 		SpringApplication.run(SampleApplication.class, args);
 	}
 
